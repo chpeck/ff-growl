baad13836d378499990d326d00eddfcd2d9290ca
Choon Hong Peck <chpeck@thingbuzz.com>
2011-02-04 21:40:35 -0800

    Added status icons and toggle.

--- content/overlay.js
+++ content/overlay.js
@@ -1,10 +1,41 @@
-window.addEventListener("load", function() { myExtension.init(); }, false);
+window.addEventListener("load", function() { extension.init(); }, false);
 var buzz_js = 'http://thingbuzz.com/embed/buzz.js';
-var myExtension = {
+var enabled = 0;
+var extension = {
   init: function() {
     var appcontent = document.getElementById("appcontent");   // browser
     if(appcontent) {
-      appcontent.addEventListener("DOMContentLoaded", myExtension.onPageLoad, true);
+      appcontent.addEventListener("DOMContentLoaded", extension.onPageLoad, true);
+    }
+    extension.toggle();
+  },
+  
+  toggle: function() {
+    enabled = (enabled + 1) % 2;
+    document.getElementById('status-bar-icon').src = 'chrome://buzzgrowl/content/icon_' + enabled + '.png';
+    extension.updateGrowl();
+  },
+  
+  updateGrowl: function() {
+    if (enabled) {
+      extension.showGrowls();
+    } else {
+      extension.hideGrowls();
+    }
+  },
+  
+  showGrowls: function() {
+    extension.injectScript();
+    var head = window.content.document.getElementsByTagName('head')[0];
+    var growl = window.content.document.createElement('script');
+    growl.innerHTML = "new TBZZ.Growl({token: '2313d3858ac9077e1429906d12fd57b1'})";
+    head.appendChild(growl);
+  },
+  
+  hideGrowls: function() {
+    var growl_divs = window.content.document.getElementsByClassName('buzz_growl');
+    for (var i=0; i<growl_divs.length; i++) {
+      growl_divs[i].parentNode.removeChild(growl_divs[i]);
     }
   },
   
@@ -17,25 +48,21 @@ var myExtension = {
     }
     return (i < script_tags.length);
   },
-  
-  injectScript: function() {
-     var head = window.content.document.getElementsByTagName('head')[0];
-     var buzz = window.content.document.createElement('script');
-     buzz.src = buzz_js;
-     head.appendChild(buzz);
 
-     var growl = window.content.document.createElement('script');
-     growl.innerHTML = "new TBZZ.Growl({token: '2313d3858ac9077e1429906d12fd57b1'})";
-     head.appendChild(growl);
+  injectScript: function() {
+    if(!extension.hasScript(buzz_js)) {
+      var head = window.content.document.getElementsByTagName('head')[0];
+      var buzz = window.content.document.createElement('script');
+      buzz.src = buzz_js;
+      head.appendChild(buzz);
+    }
   },
 
   onPageLoad: function(aEvent) {
     var doc = aEvent.originalTarget; // doc is document that triggered "onload" event
-    if(doc == window.content.document) {
+    if(enabled && doc == window.content.document) {
       if (doc.location.protocol == 'http:') {
-        if (!myExtension.hasScript(buzz_js)) {
-          myExtension.injectScript();
-        }
+        extension.updateGrowl();
       }
     }
   },
