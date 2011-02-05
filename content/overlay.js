ce0a5d22774cb9046a8936eebeb6af841220be9d
Choon Hong Peck <chpeck@thingbuzz.com>
2011-02-04 18:32:23 -0800

    Workable version of buzzgrowl without ability to turn off and on.

---
+++ content/overlay.js
@@ -0,0 +1,41 @@
+window.addEventListener("load", function() { myExtension.init(); }, false);
+var buzz_js = 'http://thingbuzz.com/embed/buzz.js';
+var myExtension = {
+  init: function() {
+    var appcontent = document.getElementById("appcontent");   // browser
+    if(appcontent)
+    appcontent.addEventListener("DOMContentLoaded", myExtension.onPageLoad, true);
+  },
+  
+  hasScript: function(script_url) {
+    var script_tags = content.document.getElementsByTagName('script');
+    for (var i=0; i<script_tags.length; i++) {
+      if (script_tags[i].src == script_url) {
+        break;
+      }
+    }
+    return (i < script_tags.length);
+  },
+  
+  injectScript: function() {
+    var head = content.document.getElementsByTagName('head')[0];
+     var buzz = content.document.createElement('script');
+     buzz.src = buzz_js;
+     head.appendChild(buzz);
+
+     var growl = content.document.createElement('script');
+     growl.innerHTML = "new TBZZ.Growl({token: '2313d3858ac9077e1429906d12fd57b1'})";
+     head.appendChild(growl);
+  },
+
+  onPageLoad: function(aEvent) {    
+    var doc = aEvent.originalTarget; // doc is document that triggered "onload" event
+    if (!myExtension.hasScript(buzz_js)) {
+      myExtension.injectScript();
+    }
+  },
+  
+  onPageUnload: function(aEvent) {
+    // do something
+  }
+}
