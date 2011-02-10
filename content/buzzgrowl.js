f6645cbdc76aa8947a75e7701d25156ca9ccde62
Seggy Umboh <seggy.umboh@gmail.com>
2011-02-09 23:44:49 -0800

    Plaster our name everywhere and protect the namespace

--- content/buzzgrowl.js
+++ content/buzzgrowl.js
@@ -1,50 +1,56 @@
-var buzz_js = 'http://buzzgrowl.com/embed/buzz.js';
-var old_buzz_js = 'http://thingbuzz.com/embed/buzz.js';
-var prefs = Components.classes['@mozilla.org/preferences-service;1'].getService(Components.interfaces.nsIPrefService).getBranch('extensions.buzzgrowl.');
-var extension = {
+(function() {
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+
+top.BuzzGrowl = {
+  prefs: Cc['@mozilla.org/preferences-service;1'].getService(Ci.nsIPrefService).getBranch('extensions.buzzgrowl.'),
+  buzz_js: 'http://buzzgrowl.com/embed/buzz.js',
+  old_buzz_js: 'http://thingbuzz.com/embed/buzz.js',
+
   init: function() {
     var appcontent = document.getElementById('appcontent');
     if(appcontent) {
-      appcontent.addEventListener('DOMContentLoaded', extension.onPageLoad, true);
+      appcontent.addEventListener('DOMContentLoaded', BuzzGrowl.onPageLoad, true);
     }
-    prefs.QueryInterface(Components.interfaces.nsIPrefBranch2);
-    if (prefs.getPrefType('enabled') == 0) {
-      prefs.setBoolPref('enabled', true);
+    BuzzGrowl.prefs.QueryInterface(Ci.nsIPrefBranch2);
+    if (BuzzGrowl.prefs.getPrefType('enabled') == 0) {
+      BuzzGrowl.prefs.setBoolPref('enabled', true);
     }
-    prefs.setCharPref('currentVersion', extension.getVersion('apps@thingbuzz.com'));
-    prefs.addObserver('enabled', this, false);
-    extension.updateIcon();
+    BuzzGrowl.prefs.setCharPref('currentVersion', BuzzGrowl.getVersion('apps@thingbuzz.com'));
+    BuzzGrowl.prefs.addObserver('enabled', this, false);
+    BuzzGrowl.updateIcon();
   },
 
   getVersion: function(addonID) {
-    var extMan = Components.classes['@mozilla.org/extensions/manager;1'].getService(Components.interfaces.nsIExtensionManager);
+    var extMan = Cc['@mozilla.org/extensions/manager;1'].getService(Ci.nsIExtensionManager);
     var ext = extMan.getItemForID(addonID);
-    ext.QueryInterface(Components.interfaces.nsIUpdateItem);
+    ext.QueryInterface(Ci.nsIUpdateItem);
     return ext.version;
   },
 
   observe: function(aSubject, aTopic, aData) {
     if ('nsPref:changed' == aTopic) {
       if(aData == 'enabled') {
-        extension.updateIcon();
-        extension.updateGrowl();
+        BuzzGrowl.updateIcon();
+        BuzzGrowl.updateGrowl();
       }
     }
   },
 
   updateIcon: function() {
-    document.getElementById('status-bar-icon').src = 'chrome://buzzgrowl/content/icon_' + (prefs.getBoolPref('enabled') ? '1' : '0') + '.png';
+    document.getElementById('status-bar-icon').src = 'chrome://buzzgrowl/content/icon_' + (BuzzGrowl.prefs.getBoolPref('enabled') ? '1' : '0') + '.png';
   },
 
   toggle: function() {
-    prefs.setBoolPref('enabled', !prefs.getBoolPref('enabled'));
+    BuzzGrowl.prefs.setBoolPref('enabled', !BuzzGrowl.prefs.getBoolPref('enabled'));
   },
 
   updateGrowl: function() {
-    if (prefs.getBoolPref('enabled')) {
-      extension.showGrowls();
+    if (BuzzGrowl.prefs.getBoolPref('enabled')) {
+      BuzzGrowl.showGrowls();
     } else {
-      extension.hideGrowls();
+      BuzzGrowl.hideGrowls();
     }
   },
 
@@ -59,8 +65,8 @@ var extension = {
 
   showGrowls: function() {
     if(window.content.document.location.protocol == 'http:' && !window.content.location.hostname.match(/(?:^|\.)(?:facebook|twitter)\.com$/)) {
-      extension.injectScript();
-      extension.newGrowl();
+      BuzzGrowl.injectScript();
+      BuzzGrowl.newGrowl();
     }
   },
 
@@ -82,22 +88,25 @@ var extension = {
   },
 
   injectScript: function() {
-    if(!extension.hasScript(buzz_js) && !extension.hasScript(old_buzz_js)) {
+    if(!BuzzGrowl.hasScript(BuzzGrowl.buzz_js) && !BuzzGrowl.hasScript(BuzzGrowl.old_buzz_js)) {
       var head = window.content.document.getElementsByTagName('head')[0];
       var buzz = window.content.document.createElement('script');
-      buzz.src = buzz_js;
+      buzz.src = BuzzGrowl.buzz_js;
       head.appendChild(buzz);
     }
   },
 
   onPageLoad: function(aEvent) {
     var doc = aEvent.originalTarget; // doc is document that triggered "onload" event
-    if (prefs.getBoolPref('enabled') && doc == window.content.document) {
-      extension.updateIcon();
-      if(!extension.hasScript(buzz_js) && !extension.hasScript(old_buzz_js)) {
-        extension.updateGrowl();
+    if (BuzzGrowl.prefs.getBoolPref('enabled') && doc == window.content.document) {
+      BuzzGrowl.updateIcon();
+      if(!BuzzGrowl.hasScript(BuzzGrowl.buzz_js) && !BuzzGrowl.hasScript(BuzzGrowl.old_buzz_js)) {
+        BuzzGrowl.updateGrowl();
       }
     }
   },
 }
-window.addEventListener("load", function() { extension.init(); }, false);
+
+window.addEventListener("load", BuzzGrowl.init, false);
+
+})();
